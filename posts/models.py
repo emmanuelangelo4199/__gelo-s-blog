d0c34ba393c68b2d3ff7fd393730dc3037362a7a
emmanuelangelo4199 <emmanuelmezie578@gmai.com>
2026-01-26 21:50:57 +0000

    deleted-row-reporter

--- posts/models.py
+++ posts/models.py
@@ -1,15 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Reporter(models.Model):
-    name = models.CharField(max_length=100)
+    # a variable to store username in the users class
+    # user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField()
     profile_pic = models.ImageField(upload_to='reporter/profile_pic/')
-    email = models.EmailField(unique=True)
+
+    # deleted email and name bbecause both exists in the Users class 
+    # Inheritance is been used here 
     
     def __str__(self):
-        return self.name 
+        # making reference to the username in the Users table
+        return self.user.username 
 
 
 class Categories(models.Model):
@@ -48,7 +54,11 @@ class Video(models.Model):
         return f"Video for {self.article.headline}"
     
     
-
-
-
-
+class Comments(models.Model):
+    article = models.ForeignKey(Articles, on_delete=models.CASCADE, related_name='comm')
+    author_name = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"Commented by {self.author_name} on {self.article.headline}"
