0e8fa3a7dc1c9aa188a5a1fba1544fe8be0d24cc
emmanuelangelo4199 <emmanuelmezie578@gmai.com>
2026-02-04 04:10:41 +0000

    navbar-desktop

--- main/views.py
+++ main/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
-
 from django.contrib import admin
 
 # Create your views here.
 
 def index(request):
-    # title = "this is my title"
-    # context = title
-    return render(request, 'main/index.html')
+    title = "this is my title"
+    context = {'title': title}
+    return render(request, 'main/index.html', context)
