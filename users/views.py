853b2a7236ebe6aff3db6eabf01d09ca5d993fb0
emmanuelangelo4199 <emmanuelmezie578@gmail.com>
2026-05-13 00:16:33 +0000

    user database

--- users/views.py
+++ users/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+
+
 
 def signup(request):
+    form = UserCreationForm()
 
-    context = {}
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+
+    context = {'form':form}
     return render(request, 'users/signup.html', context)
